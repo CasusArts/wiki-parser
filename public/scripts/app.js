b4eb51309a09d0a2185a3396c56cf19252615a9a
Fiodor Gorobet <casusarts@gmail.com>
2018-04-22 01:31:13 +0200

    add bootstrap framework

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,31 +1,26 @@
-$(document).ready(function() {
-  $('.search').click(function() {
-    const query = $(".query").val();
-    $.ajax({
-      type: "GET",
-      url: "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=" + encodeURIComponent(query) + "&callback=?", //encodeURIComponent(query)
-      contentType: "applicatino/json; character=utf-8",
-      async: true,
-      dataType: "json"
-    })
-      .done((json) => {
-        //console.log(JSON.stringify(json));
-        const markup = json.parse.text["*"];
-        const blurb = $('<div></div>').html(markup);
-        $(".results").html($(blurb).find('p'));
-      })
-      .fail(function(error) {
-        console.error("ERRROR!!!");
-      });
-    });
+$(document).ready(() => {
+  $('#query').keypress((event) => {
+    if (event.keyCode == 13) {
+      $('#go').click();
+    }
+  })
 });
 
-function getData(json) {
-  const markup = json.parse.text["*"];
-  const blurb = $('<div></div>').html(markup);
-  $(".results").html($(blurb).find('p'));
-}
-
-function reportError(error) {
-  $(".results").html("<strong>Error, please read detailed descriptio<br></strong>" + error);
-}
+$('#go').click(() => {
+  const query = $("#query").val();
+  $.ajax({
+    type: "GET",
+    url: "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=" + encodeURIComponent(query) + "&callback=?",
+    contentType: "applicatino/json; character=utf-8",
+    async: true,
+    dataType: "json"
+  }).done((json) => {
+      const markup = json.parse.text["*"];
+      const blurb = $('<div></div>').html(markup);
+      $(".results").html($(blurb).find('p'));
+  }).fail((error) => {
+      const blurb = $('<div></div>').html(error);
+      $(".results").html($(blurb).find('p'));
+      console.error("Error: " + error);
+  });
+});
